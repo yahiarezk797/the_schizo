b7e09673d1d152aee166a779299ab36274220909
yahiarezk797 <yahiarezk797@gmail.com>
2025-07-19 21:07:05 +0300

    starting an asteroids game project

---
+++ main.py
@@ -0,0 +1,10 @@
+import constants
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()

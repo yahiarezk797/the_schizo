918ae57580a031b2c6dc96c991a100e87bdde39b
yahiarezk797 <yahiarezk797@gmail.com>
2025-07-19 21:19:55 +0300

    make the black screen ready

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import constants
+import pygame
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
-
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()

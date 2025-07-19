138b6d144b7c8c6e3c8aaa1d4409b17c68f77d6b
yahiarezk797 <yahiarezk797@gmail.com>
2025-07-19 21:35:52 +0300

    make the screen 60 fps

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ def main():
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    fps = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = fps.tick(60) / 1000
 
 
 

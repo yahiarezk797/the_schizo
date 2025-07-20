ab8e4af6ab7fcde29db38ed3314692c7dd7d27bd
yahiarezk797 <yahiarezk797@gmail.com>
2025-07-20 07:39:05 +0300

    can rotate the player

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def main():
         screen.fill("black")
         player1.draw(screen)
         pygame.display.flip()
+        player1.update(dt)
         dt = fps.tick(60) / 1000
 
 
--- player.py
+++ player.py
@@ -15,3 +15,12 @@ class player(CircleShape):
         return [a, b, c]
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
